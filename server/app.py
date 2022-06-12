17301163f5dd79bca2c1951b0f17998d6c77e2b1
rumeza <rumeza06@gmail.com>
2022-06-12 06:11:04 -0600

    commits

--- server/app.py
+++ server/app.py
@@ -18,38 +18,33 @@ end_point = "http://127.0.0.1:5000/home"
 #main route
 @app.route('/')
 def main():  # put application's code here
-    return render_template('login.html', **locals())
+    return render_template('index.html')
 
-#authorization
-@app.route('/auth')
-def auth():
-    #authorization
-    return redirect(f"https://www.eventbrite.com/oauth/authorize?response_type=token&client_id={key}&redirect_uri={end_point}")
 
 #intial step in verifying auth and getting personal acess token
 @app.route('/home', methods=['POST','GET'])
 def oauth2():
     #gets the token from url via js and rediects to main screen
-    return '''  <script type="text/javascript">
-                var token = window.location.href.split("access_token=")[1]; 
-                window.location = "/main/" + token;
-            </script> '''
+    return render_template('index.html')
+
 
 #main page w/ calendar and w/ acesc token being used as variable in route
 @app.route('/main/<token>', methods=['GET','POST'])
 def view_cal(token):
     #if method pot, get eventbrite vents using token and add events via GCal
-    if request.method == 'POST':
-        #add events to GCal
-        GCal().add_events(Events(token).get_events())
+    #add events to GCal
+    GCal().add_events(Events(token).get_events())
 
 
-    return render_template('home.html')
+    return render_template('index.html')
 
 
+    
+    
+    
 @app.route('/view')
-def cal():
-    return render_template('view.html')
+def view():
+    return render_template('index.html')
 
 #runs the program
 if __name__ == '__main__':
