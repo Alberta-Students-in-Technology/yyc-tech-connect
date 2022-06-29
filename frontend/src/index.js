9bd5c1bd16019b89d096f084e8fcf9d14c7a0bd5
rumeza <rumeza06@gmail.com>
2022-06-29 14:29:04 -0600

    favicon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+import c from './favicon/1.png';
+import a from './favicon/5.png';
+import l from './favicon/3.png';
+import g from './favicon/4.png';
+import r from './favicon/6.png';
+import y from './favicon/7.png';
+
 import App from './App';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import CalendarPage from './components/CalendarPage';
 import SuccessBox from './components/SuccessBox';
+import Favicon from 'react-favicon';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Favicon url={[c,a,l,g,a,r,y]}/>
     <Router>
     <Routes>
     <Route  path='/' exact element = {<App />} />
